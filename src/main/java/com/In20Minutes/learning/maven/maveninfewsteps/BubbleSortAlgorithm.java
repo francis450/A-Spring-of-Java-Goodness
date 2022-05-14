aef11db3739cdd68c14f423bc8a0bc47d34abf9c
francis kamande <franciskamande2001@gmail.com>
2022-05-15 02:49:59 +0300

    initial BubbleSortAlgorithm code

--- src/main/java/com/In20Minutes/learning/maven/maveninfewsteps/BubbleSortAlgorithm.java
+++ src/main/java/com/In20Minutes/learning/maven/maveninfewsteps/BubbleSortAlgorithm.java
@@ -3,7 +3,7 @@ package com.In20Minutes.learning.maven.maveninfewsteps;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component // Tells Spring to manage beans from this class
 @Primary // Incase there are a number of Sort algorithms, the BubbleSortAlgorithm will be utilized first
 public class BubbleSortAlgorithm implements SortingAlgorithm {
     public int[] sort(int [] numbers){
