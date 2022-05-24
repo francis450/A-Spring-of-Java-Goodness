80908fc9228b893a5833344770be8af7fe496afa
francis kamande <franciskamande2001@gmail.com>
2022-05-24 14:04:56 +0300

    Importing qualifier from the spring framework

--- src/main/java/com/In20Minutes/learning/maven/maveninfewsteps/BubbleSortAlgorithm.java
+++ src/main/java/com/In20Minutes/learning/maven/maveninfewsteps/BubbleSortAlgorithm.java
@@ -1,5 +1,6 @@
 package com.In20Minutes.learning.maven.maveninfewsteps;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
