272dafea5de02e83314de88418b18840d5ad467b
francis kamande <franciskamande2001@gmail.com>
2022-05-24 14:04:13 +0300

    @Qualifier - used to uniquely identify Beans for easier dependency injection

--- src/main/java/com/In20Minutes/learning/maven/maveninfewsteps/BubbleSortAlgorithm.java
+++ src/main/java/com/In20Minutes/learning/maven/maveninfewsteps/BubbleSortAlgorithm.java
@@ -4,7 +4,8 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component // Tells Spring to manage beans from this class
-@Primary // Incase there are a number of Sort algorithms, the BubbleSortAlgorithm will be utilized first
+//@Primary // Incase there are a number of Sort algorithms, the BubbleSortAlgorithm will be utilized first
+@Qualifier("bubble") //If during injection, we use the @Qualifier("bubble") this component is injected first
 public class BubbleSortAlgorithm implements SortingAlgorithm {
     public int[] sort(int [] numbers){
         //logic for BUbbleSortAlgorithm goes here
