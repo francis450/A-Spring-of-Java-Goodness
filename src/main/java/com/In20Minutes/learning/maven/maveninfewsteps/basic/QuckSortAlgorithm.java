2f2cdd5c94383c5949679207a6e74fcee39d8263
francis kamande <franciskamande2001@gmail.com>
2022-05-24 21:17:21 +0300

    MOved the class into a new package named basic. And corrected a typo

--- src/main/java/com/In20Minutes/learning/maven/maveninfewsteps/basic/QuckSortAlgorithm.java
+++ src/main/java/com/In20Minutes/learning/maven/maveninfewsteps/basic/QuckSortAlgorithm.java
@@ -1,10 +1,10 @@
-package com.In20Minutes.learning.maven.maveninfewsteps;
+package com.In20Minutes.learning.maven.maveninfewsteps.basic;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("qucik")
+@Qualifier("quick")
 public class QuckSortAlgorithm implements SortingAlgorithm{
     public int[] sort(int [] numbers){
         //Quicksort algorithm
