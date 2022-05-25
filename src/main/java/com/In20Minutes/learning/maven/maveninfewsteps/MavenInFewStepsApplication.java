bb88b2c13afc7c733599e370b0b216d43bdb70b0
francis kamande <franciskamande2001@gmail.com>
2022-05-25 11:50:53 +0300

    Importing all logger classes to better understand bean creation and management

--- src/main/java/com/In20Minutes/learning/maven/maveninfewsteps/MavenInFewStepsApplication.java
+++ src/main/java/com/In20Minutes/learning/maven/maveninfewsteps/MavenInFewStepsApplication.java
@@ -1,5 +1,8 @@
 package com.In20Minutes.learning.maven.maveninfewsteps;
 
+import com.In20Minutes.learning.maven.maveninfewsteps.scope.PersonDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
